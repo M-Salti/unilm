18314f115f4422ce4fc4460ad8c177cd999e85bf
Mohamed Al Salti <mohamed.al.salti.96@gmail.com>
2020-09-02 16:26:55 +0300

    load state_dict to cpu

--- s2s-ft/s2s_ft/modeling_decoding.py
+++ s2s-ft/s2s_ft/modeling_decoding.py
@@ -784,7 +784,8 @@ class PreTrainedBertModel(nn.Module):
         model = cls(config, *inputs, **kwargs)
         if state_dict is None:
             weights_path = os.path.join(pretrained_model_name, WEIGHTS_NAME)
-            state_dict = torch.load(weights_path)
+            map_location = torch.device('cpu') if torch.cuda.is_available() else None
+            state_dict = torch.load(weights_path, map_location)
 
         old_keys = []
         new_keys = []
